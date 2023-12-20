146985d6105b939d88a4ff8efad485c44b6e7eb8
J2Y98 <qjadj98@gmail.com>
2023-12-20 14:27:44 +0900

    impl 세팅

--- ePle-MES/src/main/java/com/itwillbs/persistence/FacilityDAOImpl.java
+++ ePle-MES/src/main/java/com/itwillbs/persistence/FacilityDAOImpl.java
@@ -1,8 +1,13 @@
 package com.itwillbs.persistence;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class FacilityDAOImpl implements FacilityDAO {
-
+	private final String NAMESPACE = "com.itwillbs.mapper.FacilityMapper";
+	@Inject
+	private SqlSession sqlSession;
 }
--- ePle-MES/src/main/java/com/itwillbs/persistence/MaintenanceDAOImpl.java
+++ ePle-MES/src/main/java/com/itwillbs/persistence/MaintenanceDAOImpl.java
@@ -1,8 +1,13 @@
 package com.itwillbs.persistence;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MaintenanceDAOImpl implements MaintenanceDAO{
-
+	private final String NAMESPACE = "com.itwillbs.mapper.MaintenanceMapper";
+	@Inject
+	private SqlSession sqlSession;
 }
