32929d8e28c51d32f478e098c1e4dc14b5ab2a06
slowseo <hanononon@gmail.com>
2024-01-03 17:39:52 +0900

    returns ing

--- ePle-MES/src/main/java/com/itwillbs/domain/ReturnsVO.java
+++ ePle-MES/src/main/java/com/itwillbs/domain/ReturnsVO.java
@@ -5,4 +5,21 @@ import lombok.Data;
 @Data
 public class ReturnsVO {
 	//returns 
+	private String code;
+	private String request_code;
+	private String ship_code;
+	private String lot; //lot번호
+	private String date; //반품일자
+	private int amount; //반품수량
+	private String unit;
+	private String reason;
+	private String status; //반품상태 (반품, 폐기)
+	private int dispose; // 폐기여부
+	
+	private String reg_date;
+	private String reg_emp;
+	private String update_date;
+	private String update_emp;
+	
+	
 }
