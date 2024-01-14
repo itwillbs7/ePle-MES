8f6ca67fd14f96d769722fe4fd7c77f8124397ed
YJ <YJ@DESKTOP-IK7CGG3>
2024-01-14 14:32:51 +0900

    메인 페이지 구현

--- ePle-MES/src/main/java/com/itwillbs/persistence/MainDAO.java
+++ ePle-MES/src/main/java/com/itwillbs/persistence/MainDAO.java
@@ -1,10 +1,13 @@
 package com.itwillbs.persistence;
 
+import java.util.HashMap;
+import java.util.List;
+
 import com.itwillbs.domain.UserVO;
 
 public interface MainDAO {
 	
 	public UserVO getUserInfo(UserVO uvo) throws Exception;
 	
-	
+	public List<HashMap<String, Object>> getWeeklyProduction() throws Exception;
 }
